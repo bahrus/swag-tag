10d0e5febefc9da9c567a6ad6c1cd75da5710a87
bahrus <anderson.bruce.b@gmail.com>
2019-07-08 22:19:30 -0400

    ...

--- swag-tag.js
+++ swag-tag.js
@@ -1,3 +1,39 @@
-import { SwagTagBase } from "./swag-tag-base";
-export class SwagTag extends SwagTagBase {
+import { SwagTagBase } from "./swag-tag-base.js";
+import { define } from "trans-render/define.js";
+import { append } from 'trans-render/append.js';
+import { createTemplate } from "xtal-element/utils.js";
+const styleTemplate = createTemplate(
+/* html */ `
+<style>
+:host{
+    display: block;
 }
+details>summary {
+    margin-top: 20px;
+    list-style: none;
+    cursor:pointer;
+}
+details>summary::-webkit-details-marker{
+    display:none;
+}
+</style>
+`);
+export class SwagTag extends SwagTagBase {
+    static get is() {
+        return "swag-tag";
+    }
+    get noShadow() {
+        return false;
+    }
+    get renderOptions() {
+        if (this._renderOptions === undefined) {
+            this._renderOptions = {
+                initializedCallback: (ctx, target) => {
+                    append(target, styleTemplate);
+                }
+            };
+        }
+        return this._renderOptions;
+    }
+}
+define(SwagTag);
--- swag-tag.ts
+++ swag-tag.ts
@@ -1,5 +1,44 @@
-import { SwagTagBase } from "./swag-tag-base";
+import { SwagTagBase } from "./swag-tag-base.js";
+import { define } from "trans-render/define.js";
+import {RenderOptions, RenderContext} from 'trans-render/init.d.js';
+import {append} from 'trans-render/append.js';
+import { createTemplate } from "xtal-element/utils.js";
 
-export class SwagTag extends SwagTagBase{
-    
+const styleTemplate = createTemplate(
+/* html */ `
+<style>
+:host{
+    display: block;
 }
+details>summary {
+    margin-top: 20px;
+    list-style: none;
+    cursor:pointer;
+}
+details>summary::-webkit-details-marker{
+    display:none;
+}
+</style>
+`);
+export class SwagTag extends SwagTagBase {
+  static get is() {
+    return "swag-tag";
+  }
+
+  get noShadow() {
+    return false;
+  }
+
+  _renderOptions: RenderOptions | undefined; 
+  get renderOptions(): RenderOptions {
+      if(this._renderOptions === undefined){
+          this._renderOptions = {
+            initializedCallback:(ctx: RenderContext, target: HTMLElement | DocumentFragment) =>{
+                append(target, styleTemplate)
+            }
+          } as RenderOptions;
+      }
+      return this._renderOptions;
+  }
+}
+define(SwagTag);
