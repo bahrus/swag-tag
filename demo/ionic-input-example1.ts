1a441ba4f7cda0f73ba1304061f9badbe1313e25
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-08-05 22:19:50 -0400

    Fail:  es-dev-server + ionic gives error Uncaught (in promise) SyntaxError: The requested module '../../../../../@stencil/core/internal/stencil-core/index.js' does not provide an export named 'getMode'

---
+++ demo/ionic-input-example1.ts
@@ -0,0 +1,9 @@
+import {Input} from '@ionic/core/dist/collection/components/input/input.js';
+
+/**
+ * @element ionic-input
+ */
+export class IonicInputExample1 extends Input{
+
+}
+customElements.define('ionic-input-example1', IonicInputExample1);
